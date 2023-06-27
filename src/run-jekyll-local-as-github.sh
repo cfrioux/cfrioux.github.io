d56716b95918641d9a626f42536a026859b7acbc
cfrioux <clemence.frioux@gmail.com>
2023-06-27 08:35:26 +0200

    update content

--- src/run-jekyll-local-as-github.sh
+++ src/run-jekyll-local-as-github.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+# if problems with the version of ruby
+# chruby 2.7.2
+# then 
+# bundle install
+# see https://open-research.gemmadanks.com/tutorials/how-to-use-jekyll-scholar-with-github-pages/
 export LANG=en_US.UTF-8
 bundle exec jekyll serve --config _config.yml $1
